1692ffebfb715a7f45bf2e427e7e03024e8f912a
Daniel Vaz Calaca <107972303+danielvazcalaca@users.noreply.github.com>
2024-10-09 10:24:06 -0300

    add ex013

---
+++ exercises/ex013/play.js
@@ -0,0 +1,11 @@
+const nome = 'Maxsteel'
+let age = 123
+let hobby = false
+
+function abobora(userName, userAge, userHobby) {
+  return (
+    'Name is ' + userName + ', age is ' + userAge + ', hobby is ' + userHobby
+  )
+}
+
+console.log(abobora(nome, age, hobby))
