564a076d98b0f8a4040fca00219a9593c90df364
Daniel Vaz Calaca <107972303+danielvazcalaca@users.noreply.github.com>
2024-10-14 16:56:55 -0300

    add ex015

---
+++ exercises/ex015/script.js
@@ -0,0 +1,10 @@
+let num1 = 9.5
+num2 = Math.floor(num1)
+num3 = Math.ceil(num1 + 5)
+num4 = Math.round(num1)
+
+console.log(num2, num3, num4)
+console.log(Math.max(num2, num1, num3, num4))
+
+let random = Math.round(Math.random() * (10 - 5) + 5)
+console.log(random)
